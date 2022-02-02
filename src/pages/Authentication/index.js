60876ce92adf7e4a2acff971c9ae15680b6ac179
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-01 20:48:14 -0500

    Update Authentication page with MUI style and add Roboto font to global project

--- src/pages/Authentication/index.js
+++ src/pages/Authentication/index.js
@@ -1,29 +1,60 @@
 import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const Authentication = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log('values', email, password);
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button>Login</button>
-      </form>
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      sx={{ mt: 4 }}
+    >
+      <Typography component="h2" variant="h2">
+        Sign in
+      </Typography>
+      <Box component="form" sx={{ mt: 3, width: 500 }} onSubmit={handleSubmit}>
+        <Stack spacing={2}>
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Stack>
+        <Button
+          variant="contained"
+          disableElevation
+          size="large"
+          sx={{ mt: 3 }}
+          type="submit"
+        >
+          Login
+        </Button>
+      </Box>
+    </Grid>
   );
-};
+}
 
 export default Authentication;
