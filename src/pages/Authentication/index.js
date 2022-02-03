fea3fc4ec4e9189fafd7e3acb7354d772aea481d
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-02 19:27:55 -0500

    Add axios and use useEffect to submit basic login form

--- src/pages/Authentication/index.js
+++ src/pages/Authentication/index.js
@@ -1,19 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import axios from 'axios';
 
 const Authentication = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setIsSubmitting(true);
     console.log('values', email, password);
   };
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      return;
+    }
+
+    console.log('Effect!');
+
+    axios.post('http://localhost:3000/api/users/login', {
+      user: {
+        email: 'fadi@email.com',
+        password: 'somepassword',
+      },
+    })
+      .then((res) => {
+        setIsSubmitting(false);
+        console.log('res', res);
+      })
+      .catch((err) => {
+        setIsSubmitting(false);
+        console.log('err', err);
+      });
+
+    document.title = email;
+  });
 
   return (
     <Grid
@@ -49,6 +78,7 @@ const Authentication = () => {
           size="large"
           sx={{ mt: 3 }}
           type="submit"
+          disabled={isSubmitting}
         >
           Login
         </Button>
