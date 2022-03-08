2dd2f085a60e35ba689cbf9f27bff9a506431c80
Fadi Serapian <fadi.serapian@gmail.com>
2022-03-07 20:22:18 -0500

    Add global feed and base feed with fetched articles

---
+++ src/components/feed.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+
+const Feed = ({ articles }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      {articles.map((article) => (
+        <Card sx={{ minWidth: 275, mb: 3 }} key={article.id}>
+          <CardContent>
+            <Box sx={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
+              <Link
+                to={`profiles/${article.author.username}`}
+                style={{ display: 'flex', alignItems: 'center' }}
+              >
+                <img
+                  src={article.author.image}
+                  alt=""
+                  style={{ width: '2rem', borderRadius: '50%' }}
+                />
+              </Link>
+              <Link
+                to={`profiles/${article.author.username}`}
+                style={{ color: '#000', textDecoration: 'none' }}
+              >
+                <Typography>{article.author.username}</Typography>
+              </Link>
+            </Box>
+            <Typography variant="caption">{article.createdAt}</Typography>
+            <Typography variant="h5">{article.title}</Typography>
+            <Typography variant="p" style={{ color: '#777' }}>
+              {article.description}
+            </Typography>
+          </CardContent>
+          <CardActions
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Link to={`articles/${article.slug}`}>
+              <Button size="small">Learn More</Button>
+            </Link>
+            <Box sx={{ display: 'flex', gap: '1rem' }}>
+              {article.tagList.map((tag) => (
+                <Paper
+                  elevation={0}
+                  variant="outlined"
+                  xs={{ padding: '1rem' }}
+                >
+                  {tag}
+                </Paper>
+              ))}
+            </Box>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
+  );
+};
+
+export default Feed;
--- src/pages/globalFeed/index.js
+++ src/pages/globalFeed/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Feed from '../../components/feed';
+import useFetch from '../../hooks/useFetch';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 const GlobalFeed = () => {
+  const apiUrl = 'articles?limit=10&offset=0';
+  const [{ isLoading, response, error }, doFetch] = useFetch(apiUrl);
+
+  console.log('res', isLoading, response, error);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
   return (
-    <div>
-      <span>Global Feed</span>
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          {isLoading && <Box>Loading...</Box>}
+          {error && <Box>Something went wrong!</Box>}
+          {!isLoading && response && <Feed articles={response.articles} />}
+        </Grid>
+        <Grid item xs={4}>
+          <div>Popular tags</div>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
